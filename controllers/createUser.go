5ccbe83e65cde59d0c7db82583834f5cd173f1d6
oprogramador <uomodislesia@gmail.com>
2020-08-13 01:53:24 +0200

    remove redundant find

--- controllers/createUser.go
+++ controllers/createUser.go
@@ -16,14 +16,9 @@ import (
 
 func CreateUser(ctx context.Context, usersCollection *mongo.Collection) func(ginContext *gin.Context) {
 	return func(ginContext *gin.Context) {
-		cursor, err := usersCollection.Find(ctx, bson.D{})
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer cursor.Close(ctx)
 		reqBody, _ := ioutil.ReadAll(ginContext.Request.Body)
 		var user models.User
-		err = json.Unmarshal(reqBody, &user)
+		err := json.Unmarshal(reqBody, &user)
 		if err != nil {
 			re := regexp.MustCompile(`[A-Za-z.]* of type [A-Za-z]*`)
 			ginContext.String(400, strings.ReplaceAll(string(re.Find([]byte(err.Error()))), "of type", "should be of type"))
